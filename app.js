d7f5e7dc283c06e62a1116d6be3fd75942bae4e4
David Petryca <david@petryca.com>
2022-10-03 10:54:04 +0200

    class 3.10

---
+++ app.js
@@ -0,0 +1,19 @@
+(() => {
+
+    
+    const observerOptions = {
+        rootMargin: '1000px 0px 0px 0px',
+        threshold: 0.7
+    };
+
+    const observe = entries => entries.forEach(entry => {
+        entry.target.classList.toggle('inviewport', entry.isIntersecting);
+    });
+
+    const obs = new IntersectionObserver(observe, observerOptions);
+
+    document.querySelectorAll('article').forEach(el => obs.observe(el));
+
+
+
+})();
